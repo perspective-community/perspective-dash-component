f37cfc3a4ac3724fee5f8540f8ef15810f4e735a
Tim Paine <t.paine154@gmail.com>
2018-11-25 19:00:56 +0000

    init from cookiecutter

---
+++ perspective_dash_component/_imports_.py
@@ -0,0 +1,6 @@
+from .perspective import perspective
+
+
+__all__ = [
+    "perspective",
+]
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+import perspective from './components/perspective.react';
+
+export {
+    perspective
+};
