88aa7badb68226d965ec34b77bab3d1f0f491395
Tim Paine <t.paine154@gmail.com>
2018-11-29 14:41:34 -0500

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,35 @@
-const path = require('path');
-const packagejson = require('./package.json');
+const path = require("path");
+const PerspectivePlugin = require("@jpmorganchase/perspective/webpack-plugin");
+const webpack = require("webpack");
 
-const dashLibraryName = packagejson.name.replace(/-/g, '_');
-
-module.exports = (env, argv) => {
-
-    let mode;
-
-    const overrides = module.exports || {};
-
-    // if user specified mode flag take that value
-    if (argv && argv.mode) {
-        mode = argv.mode;
-    }
-
-    // else if configuration object is already set (module.exports) use that value
-    else if (overrides.mode) {
-        mode = overrides.mode;
-    }
-
-    // else take webpack default (production)
-    else {
-        mode = 'production';
-    }
-
-    let filename = (overrides.output || {}).filename;
-    if(!filename) {
-        const modeSuffix = mode === 'development' ? 'dev' : 'min';
-        filename = `${dashLibraryName}.${modeSuffix}.js`;
-    }
-
-    const entry = overrides.entry || {main: './src/lib/index.js'};
-
-    const devtool = overrides.devtool || (
-        mode === 'development' ? "eval-source-map" : 'none'
-    );
-
-    const externals = ('externals' in overrides) ? overrides.externals : ({
-        react: 'React',
-        'react-dom': 'ReactDOM',
-        'plotly.js': 'Plotly',
-    });
-
-    return {
-        mode,
-        entry,
-        output: {
-            path: path.resolve(__dirname, dashLibraryName),
-            filename,
-            library: dashLibraryName,
-            libraryTarget: 'window',
-        },
-        externals,
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                    },
+module.exports = {
+    entry: "./src/lib/index.js",
+    resolveLoader: {
+        alias: {
+            "file-worker-loader": "@jpmorganchase/perspective/src/loader/file_worker_loader.js"
+        }
+    },
+    externals: /\@jupyter|\@phosphor|react|react-dom|plotly.js/,
+    plugins: [new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /(en|es|fr)$/), new PerspectivePlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{loader: "css-loader"}]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
                 },
-                {
-                    test: /\.css$/,
-                    use: [
-                        {
-                            loader: 'style-loader',
-                        },
-                        {
-                            loader: 'css-loader',
-                        },
-                    ],
-                },
-            ],
-        },
-        devtool
+            },
+            {test: /\.ts?$/, loader: "ts-loader"}
+        ]
+    },
+    output: {
+        filename: "index.js",
+        libraryTarget: "umd",
+        path: path.resolve(__dirname, "../../build")
     }
 };
