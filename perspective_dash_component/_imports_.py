d2365197217894f3521005d9f5ef7295b926db36
Tim Paine <t.paine154@gmail.com>
2018-12-05 11:36:11 -0500

    working on #2

--- perspective_dash_component/_imports_.py
+++ perspective_dash_component/_imports_.py
@@ -1,4 +1,10 @@
 from .perspective_dash import perspective_dash
+from perspective import PerspectiveBaseMixin
+
+
+class PerspectiveDash(PerspectiveBaseMixin, perspective_dash):
+    pass
+
 
 __all__ = [
     "perspective_dash"
